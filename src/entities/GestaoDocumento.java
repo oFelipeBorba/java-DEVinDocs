2f806d77f31f9a994a662c757cdb13b74abcce4a
Felipe Borba <108702072+oFelipeBorba@users.noreply.github.com>
2022-10-24 11:45:06 -0300

    refactor

--- src/entities/GestaoDocumento.java
+++ src/entities/GestaoDocumento.java
@@ -1,13 +1,13 @@
 package entities;
 
-public class Documento {
+public class GestaoDocumento {
     private Double id = Math.random();
     private Double idResponsavel;
     private Double idCriador;
     private String link;
     private String estado;
 
-    public Documento(Double idResponsavel, Double idCriador, String link, String estado) {
+    public GestaoDocumento(Double idResponsavel, Double idCriador, String link, String estado) {
         this.idResponsavel = idResponsavel;
         this.idCriador = idCriador;
         this.link = link;
